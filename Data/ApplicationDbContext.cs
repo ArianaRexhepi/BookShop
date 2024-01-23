2d7a182641056ebd34bc8569d4d16763c0bc81ab
Rigon Vila <121397549+RigonVila@users.noreply.github.com>
2024-01-23 21:33:14 +0100

    inputing fields

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         }
         public DbSet<Book> Books { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         
     }
--- Models/Order.cs
+++ Models/Order.cs
@@ -7,6 +7,10 @@ namespace Books.Models
 {
     public class Order
     {
-        
+        public int Id { get; set; }
+        public List<OrderItem> OrderItems{ get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+
     }
 }
