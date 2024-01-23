8fa8bc5c43a01cf97fd1ad39b2e80f4c7dfac8aa
Rigon Vila <121397549+RigonVila@users.noreply.github.com>
2024-01-23 22:01:13 +0100

    checkout functionality done

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Books.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Books.Controllers
 {
@@ -21,6 +22,26 @@ namespace Books.Controllers
         public IActionResult Checkout()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckOut(Order order)
+        {
+            var cartItems = _cart.GetAllCartItems();
+            _cart.CartItems = cartItems;
+
+            if(_cart.CartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Cart is empty, please add a book first!");
+            }
+
+            if(ModelState.IsValid)
+            {
+                CreateOrder(order);
+                _cart.ClearCart();
+                return View("Checkout Complete", order);
+            }
+            return View(order);
         }
 
         public void CreateOrder(Order order)
