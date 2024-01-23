902fed26fc7203cad9ec6f74a6d9452ea1a25938
Rigon Vila <121397549+RigonVila@users.noreply.github.com>
2024-01-23 21:48:57 +0100

    create order functionality

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,15 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Books.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly Cart _cart;
+       
+        public OrderController(ApplicationDbContext context, Cart cart)
+        {
+            _context = context;
+            _cart = cart;
+        }
+
+        public IActionResult Checkout()
         {
             return View();
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            var cartItems = _cart.CartItems;
+
+            foreach (var item in cartItems)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Quantity = item.Quantity,
+                    BookId = item.Book.Id,
+                    OrderId = order.Id,
+                    Price = (int)(item.Book.Price * item.Quantity),
+
+                };
+                order.OrderItems.Add(orderItem);
+                order.OrderTotal += orderItem.Price;
+            }
+            _context.Orders.Add(order);
+            _context.SaveChanges();
         }
     }
 }
